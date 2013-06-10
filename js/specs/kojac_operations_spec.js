2c2c7a7c4819dcbeb1d581e30d81ee6dd20e17ca
Gary McGhee <gary@buzzware.com.au>
2013-06-10 23:28:14 +0800

    working on kojac_mock_spec.js

--- js/specs/kojac_operations_spec.js
+++ js/specs/kojac_operations_spec.js
@@ -10,27 +10,27 @@ describe("Kojac Operations", function() {
 		});
 	});
 
-	it("try reading cur_super,cur_super_product", function() {
+	it("try reading order_item,product,order_item__50", function() {
 		var op;
 		var req;
 		runs(function() {
-			req = this.kojac.read('cur_super').read('cur_super_product').read('cur_super__50');
+			req = this.kojac.read('order_item').read('product').read('order_item__50');
 			expect(req).toBeDefined('ops');
 			expect(req).toBeDefined('options');
 			expect(req.ops.length).toEqual(3);
 			op = req.ops[0];
 			expect(op.verb).toEqual('READ');
-			expect(op.key).toEqual('cur_super');
+			expect(op.key).toEqual('order_item');
 			expect(op.result_key).toEqual(op.key);
 			expect(op.results).toEqual({});
 			op = req.ops[1];
 			expect(op.verb).toEqual('READ');
-			expect(op.key).toEqual('cur_super_product');
+			expect(op.key).toEqual('product');
 			expect(op.result_key).toEqual(op.key);
 			expect(op.results).toEqual({});
 			op = req.ops[2];
 			expect(op.verb).toEqual('READ');
-			expect(op.key).toEqual('cur_super__50');
+			expect(op.key).toEqual('order_item__50');
 			expect(op.result_key).toEqual(op.key);
 			expect(op.results).toEqual({});
 		});
