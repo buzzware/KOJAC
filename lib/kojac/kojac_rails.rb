1a5b4648b4a56e048e9ca528bb65a4ed20e10df7
Gary McGhee <gary@buzzware.com.au>
2013-08-21 07:00:59 +0800

    commented out ring_strong_parameters

--- lib/kojac/kojac_rails.rb
+++ lib/kojac/kojac_rails.rb
@@ -1,4 +1,4 @@
-require File.expand_path('ring_strong_parameters',File.dirname(__FILE__))
+#require File.expand_path('ring_strong_parameters',File.dirname(__FILE__))
 
 Kernel.class_eval do
   def key_join(aResource,aId=nil,aAssoc=nil)
